42880e2ae3e5dcabaadcd1cdfe64cca393403849
Damian Krzeminski <pirxpilot@furkot.com>
2023-09-24 18:13:13 +0200

    replace mocha with node:test runner
    this time we are using node:test runner to test the generator itself

--- test/test-creation.js
+++ test/test-creation.js
@@ -1,3 +1,4 @@
+const { describe, it, beforeEach } = require('node:test');
 const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
--- test/test-load.js
+++ test/test-load.js
@@ -1,4 +1,5 @@
-const assert  = require('assert');
+const { describe, it } = require('node:test');
+const assert  = require('node:assert');
 
 describe('node generator', () => {
   it('can be imported without blowing up', () => {
